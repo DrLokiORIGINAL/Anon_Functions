4c51285756daef990dfcb581fd0b2b15c8c9c1dd
yaroshevski <wsr1@K218PC1>
2022-04-11 12:18:23 +0300

    Я сделал лямбда-операторов

---
+++ StatementLambdas/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StatementLambdas
+{
+    public delegate void HelloDelegate(string s);
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloDelegate hD = (x) => { Console.WriteLine("Привет, " + x); };
+
+            hD("Вася");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ StatementLambdas1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StatementLambdas1
+{
+    public delegate int SubDelegate(int a, int b);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SubDelegate sD = (s, d) =>
+            {
+                int res = s - d;
+                return res;
+            };
+            int c = sD(10, 8);
+            Console.WriteLine(c);
+            Console.ReadKey();
+        }
+    }
+}
